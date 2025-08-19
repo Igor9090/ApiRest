21f16790d583757a43acac2ee3a323f9e0448f18
IgorVn <igorvinicin123@gmail.com>
2025-08-19 18:53:30 -0300

    Alterando database

--- app/src/config/database.js
+++ app/src/config/database.js
@@ -1,15 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: false,
     define: {
       timestamps: true,
       underscored: true,
       underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    }
-  }
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  },
 };
